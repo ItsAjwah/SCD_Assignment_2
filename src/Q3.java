59b777541625fb3fe0481331a5845d02f0b56479
Ajwa Asif <jiyaasif21@gmail.com>
2023-10-10 21:55:20 +0500

    Version 1.1

--- src/Q3.java
+++ src/Q3.java
@@ -1,34 +1,33 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Q3 {
-    public static List<int[]> mergeintervals(List<int[]> intervals) //takes a list of intervals represented as arrays of integers as its parameter.
-    {
+    public static void mergeintervals(List<int[]> intervals) {
         if (intervals.isEmpty()) {
-            return new ArrayList<>();
+            return;
         }
-
-        List<int[]> merged_interval = new ArrayList<>();
+        int index = 0;
         int[] curr_interval = intervals.get(0);
 
         for (int i = 1; i < intervals.size(); i++) {
             int[] nextinterval = intervals.get(i);
 
-            if (curr_interval[1] >= nextinterval[0]) { //merging curr and next interval
-                curr_interval[1] = Math.max(curr_interval[1], nextinterval[1]);
+            if (curr_interval[1] >= nextinterval[0]) { // Merging curr and next interval
+                curr_interval[1] = Math.max(curr_interval[1], nextinterval[1]);  //jo bara hoga
+                // woi num return kerna
             } else {
-                // Add the current interval to the result and update the current interval
-                merged_interval.add(curr_interval);
+                // Update the current interval  and agle index pey move hogye
+                intervals.set(index, curr_interval);  //curr_interval, index ki jagh replace ho rh
+                index++;
                 curr_interval = nextinterval;
             }
         }
 
-        // Add the last merged interval
-        merged_interval.add(curr_interval);
+        intervals.set(index, curr_interval);
+        index++;
 
-        return merged_interval;
+        intervals.subList(index, intervals.size()).clear(); //any remaining intervals in the list after the newindex are removed
     }
 
     public static void main(String[] args) {
@@ -37,7 +36,7 @@ public class Q3 {
         System.out.println("Enter intervals as pairs of start and end values. Enter '0 0' to stop.");
 
         List<int[]> intervals = new ArrayList<>();
-        int interval_count = 1; //phli iteration
+        int interval_count = 1; // 1st index for iteration
 
         while (true) {
             System.out.print("Enter interval " + interval_count + " (start end): ");
@@ -45,18 +44,18 @@ public class Q3 {
             int end = scanner.nextInt();
 
             if (start == 0 && end == 0) {
-                break; //stop hogye
+                break; // 0 0 pey stop kerdein gy
             }
 
             intervals.add(new int[]{start, end});
-            interval_count++;
+            interval_count++; //jesy ab 2 hogye ga
         }
 
-        List<int[]> mergedIntervals = mergeintervals(intervals);
+        mergeintervals(intervals);
 
-        System.out.println("Merged Intervals:");
-        for (int[] interval : mergedIntervals) {
-            System.out.println(Arrays.toString(interval));
+        for (int[] interval : intervals) {
+            System.out.println("Merged Interval: [" + interval[0] +
+                    ", " + interval[1] + "]");
         }
 
         scanner.close();
